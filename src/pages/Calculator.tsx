effa387d5ce47e785a7b6f1ee8fddb6391f51dd3
N000183926 <makfichen@fpg.com.tw>
2023-12-31 13:37:50 +0800

    修改計算機

--- src/pages/Calculator.tsx
+++ src/pages/Calculator.tsx
@@ -1,14 +1,32 @@
 import { useNavigate } from "react-router";
-import { Button } from "@mui/material";
+import { Box, Button, Card, IconButton, TextField, Typography, colors, styled } from "@mui/material";
 
 const Calculator = () => {
     const navigate = useNavigate();
     return (
-        <div>
-            Cal
+        <Box>
             <Button onClick={() => navigate("/")}>回首頁</Button>
-        </div>
+            <Box margin={1} textAlign={"center"}>
+                <Typography variant="h4" color={"default"}>一般計算機</Typography>
+                <Typography>
+                    提供簡單計算
+                </Typography>
+                
+            </Box>
+            <Card sx={{width:"300px"}}>
+                <Box textAlign={"center"}>
+                <TextField disabled />
+                </Box>
+               <Box margin={1} textAlign={"center"}>
+               <IconButton color="info"  sx={btnStyle}>A</IconButton> 
+               </Box>
+            </Card>
+        </Box>
     )
 }
 
 export default Calculator;
+
+const btnStyle = {
+    bgcolor: 'primary.main', width: 40, height: 40, color: "white",  ":hover": { color: "primary.main" },
+}
